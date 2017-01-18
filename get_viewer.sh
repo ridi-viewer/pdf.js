e95ef02197a3ee2354a00946080d70a1e4875d17
kgwangrae <k.gwangrae@ridi.com>
2017-01-18 14:39:53 +0900

    [get_viewer.sh] Make 'bundle all the things' as default (removed -f option) [skip ci]
    - Now -v option is required to bundle only viewer.

--- get_viewer.sh
+++ get_viewer.sh
@@ -1,24 +1,22 @@
 gulp clean &&
-(if [[ $1 == "-f" ]]
+(if [[ $1 == "-v" ]]
 then
-  gulp lint
-else
   gulp lintviewer
-fi) &&
-(if [[ $1 == "-f" ]]
-then
-  gulp generic
 else
+  gulp lint
+fi) &&
+(if [[ $1 == "-v" ]]
+then
   gulp genericviewer
+else
+  gulp generic
 fi) &&
 rsync -avh --delete build/generic/* ../res/pdf-viewer/ &&
 rsync -avh --delete ridi_modules ../res/pdf-viewer/ &&
 cd ../res &&
 ./generate-qrc.sh &&
-(if [[ $1 == "-f" ]]
+(if [[ $1 != "-v" ]]
 then
-  echo "NOTE : All files including build script are gone through lint."
-else
-  echo "NOTE : To build and lint all PDF.js module then give -f option to this script."
+  echo "NOTE : To build only viewer, give -v option to this script."
 fi) &&
 echo "NOTE : Qt RCC may not notice the changes sometimes."
